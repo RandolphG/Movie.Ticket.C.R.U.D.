1c64a1b6c022422ddb9656d5fbd2b9b89c343fc0
randolphg <poplogics@MSI.localdomain>
2020-03-11 03:23:40 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movies: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.movies[0] && (
+          <Table>
+            <thead>
+              <tr>
+                <th>title</th>
+                <th>rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map(movie => (
+                <tr>
+                  <td>{movie.title}</td>
+                  <td>{movie.mpaa}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    const movies = await fetch("http://localhost:3001/movies")
+      .then(res => res.json())
+      .then(data => data.movies);
+    this.setState({ movies });
+    console.log(this.state.movies);
+  }
 }
-
 export default App;
